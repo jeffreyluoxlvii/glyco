7a403469d319e026dfadc4ee68a63244896e5f88
jeffrey.luo <jeffrey.luo@duke.edu>
2020-09-15 23:25:53 -0700

    Created the various screens, and a simple navigation bar

---
+++ glyco/lib/screens/measures_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MeasuresScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Measures Screen"),
+    );
+  }
+}
