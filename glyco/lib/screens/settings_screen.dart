6fc9a3e0ce1fef6ee06084d4a289f5a4f717ac1d
Justin Wu <jaw117@duke.edu>
2020-09-24 22:10:49 -0600

    Finished main settings screen

--- glyco/lib/screens/settings_screen.dart
+++ glyco/lib/screens/settings_screen.dart
@@ -1,66 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:glyco/screens/challenges_screen.dart';
 
-class SettingsScreen extends StatefulWidget {
-  SettingsScreen({Key key}) : super(key: key);
-
+class SettingsScreen extends StatelessWidget {
   @override
-  _SettingsScreenState createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: settingsScreen(context),
-    );
-  }
-
-  Widget settingsScreen(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Icon(
-              Icons.ac_unit,
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(30, 30, 30, 5),
-          child: Text(
-            "Jessica Woodard",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-            ),
-          ),
-        ),
-        Text(
-          "jess.woodard@gmail.com",
-          style: TextStyle(
-            fontSize: 16
-          )
-        ),
-        exportData(context),
-        Text(
-          "Shortcuts",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold
-          )
-        ),
-        Text(
-          "Integrations",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold
-          )
-        ),
-      ],
-    );
-  }
-
+  
   ClipRRect exportData(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(25.0),
@@ -72,7 +14,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
         child: FlatButton(
           child: Text("EXPORT DATA",
               style: TextStyle(
-                fontSize: 20,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
                 color: Colors.white,
               )),
           onPressed: () {
@@ -82,5 +25,115 @@ class _SettingsScreenState extends State<SettingsScreen> {
       ),
     );
   }
-}
 
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(30),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(children: [
+              Spacer(),
+              Icon(
+                Icons.ac_unit,
+              ),
+              Spacer(),
+            ]),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Jessica Woodard",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "jess.woodard@gmail.com",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height:15),
+                Row(children: [
+                  Spacer(),
+                  exportData(context),
+                  Spacer(),
+                ]),
+                SizedBox(height:15),
+                Text(
+                  "Shortcuts",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                SizedBox(height:15),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.favorite,
+                          color: Theme.of(context).primaryColor,
+                          size: 45,
+                        ),
+                        Text("Manual Input"),
+                        Spacer(),
+                        Icon(
+                          Icons.fastfood,
+                          color: Theme.of(context).primaryColor,
+                          size: 45,
+                        ),
+                        Text(" 500 kcal\n 35g carbs"),
+                      ],
+                    ),
+                    SizedBox(height:15),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.donut_small,
+                          color: Theme.of(context).primaryColor,
+                          size: 45,
+                        ),
+                        Text(" 250 kcal\n 15g carbs"),
+                        Spacer(),
+                        Icon(
+                          Icons.free_breakfast,
+                          color: Theme.of(context).primaryColor,
+                          size: 45,
+                        ),
+                        Text(" 100 kcal\n 15g carbs"),
+                      ],
+                    ),
+                    SizedBox(height:15),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.directions_run,
+                          color: Theme.of(context).primaryColor,
+                          size: 45,
+                        ),
+                        Text("30 min activity"),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height:15),
+                Text(
+                  "Integrations",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+}
