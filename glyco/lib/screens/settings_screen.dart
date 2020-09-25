05db1dc4a8337c351b320dc29fb21438f65e26f2
jeffrey.luo <jeffrey.luo@duke.edu>
2020-09-24 20:22:30 -0700

    Reverted screen to original

--- glyco/lib/screens/settings_screen.dart
+++ glyco/lib/screens/settings_screen.dart
@@ -3,15 +3,8 @@ import 'package:flutter/material.dart';
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('Hello'),
-          Text('World'),
-        ],
-      ),
+    return Center(
+      child: Text("Settings Screen"),
     );
   }
 }
