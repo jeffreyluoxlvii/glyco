bab02c170571679f1cea08a5dd8af1d8331f35fe
Herleen Kaur <hk233@duke.edu>
2020-10-07 21:26:20 -0500

    tried to add averages functions but there are errors

--- glyco/lib/providers/challenges.dart
+++ glyco/lib/providers/challenges.dart
@@ -38,28 +38,118 @@ class Challenges with ChangeNotifier {
 
   // STEPS AVERAGES
   int monthSteps() {
+    // int totalSteps = 0;
+    // int numDays = 0;
+
+    // for (int i = 7; i < 31; i++) {
+    //   DateTime day = DateTime.now().subtract(Duration(days: i));
+
+    //   if (findByDate(day).steps != null) {
+    //     totalSteps += findByDate(day).steps;
+    //     numDays++;
+    //   }
+    // }
+
+    // int avgSteps = (totalSteps / numDays).round();
+    // return avgSteps;
+
     return 12487;
   }
 
   int weekSteps() {
+    // int totalSteps = 0;
+    // int numDays = 0;
+
+    // for (int i = 1; i < 7; i++) {
+    //   DateTime day = DateTime.now().subtract(Duration(days: i));
+
+    //   if (findByDate(day).steps != null) {
+    //     totalSteps += findByDate(day).steps;
+    //     numDays++;
+    //   }
+    // }
+
+    // int avgSteps = (totalSteps / numDays).round();
+    // return avgSteps;
+
     return 9587;
   }
 
   // ACTIVITY AVERAGES
   int monthActivity() {
+    // int totalActivity = 0;
+    // int numDays = 0;
+
+    // for (int i = 7; i < 31; i++) {
+    //   DateTime day = DateTime.now().subtract(Duration(days: i));
+
+    //   if (findByDate(day).exerciseTime != null) {
+    //     totalActivity += findByDate(day).exerciseTime;
+    //     numDays++;
+    //   }
+    // }
+
+    // int avgActivity = (totalActivity / numDays).round();
+    // return avgActivity;
+
     return 66;
   }
 
   int weekActivity() {
+    // int totalActivity = 0;
+    // int numDays = 0;
+
+    // for (int i = 1; i < 7; i++) {
+    //   DateTime day = DateTime.now().subtract(Duration(days: i));
+
+    //   if (findByDate(day).exerciseTime != null) {
+    //     totalActivity += findByDate(day).exerciseTime;
+    //     numDays++;
+    //   }
+    // }
+
+    // int avgActivity = (totalActivity / numDays).round();
+    // return avgActivity;
+
     return 28;
   }
 
   // CARBS AVERAGES
   int monthCarbs() {
+    // int totalCarbs = 0;
+    // int numDays = 0;
+
+    // for (int i = 7; i < 31; i++) {
+    //   DateTime day = DateTime.now().subtract(Duration(days: i));
+
+    //   if (findByDate(day).carbs != null) {
+    //     totalCarbs += findByDate(day).carbs;
+    //     numDays++;
+    //   }
+    // }
+
+    // int avgCarbs = (totalCarbs / numDays).round();
+    // return avgCarbs;
+
     return 34;
   }
 
   int weekCarbs() {
+    // int totalCarbs = 0;
+    // int numDays = 0;
+
+    // for (int i = 1; i < 7; i++) {
+    //   DateTime day = DateTime.now().subtract(Duration(days: i));
+
+    //   if (findByDate(day).carbs != null) {
+    //     totalCarbs += findByDate(day).carbs;
+    //     numDays++;
+    //   }
+    // }
+
+    // int avgCarbs = (totalCarbs / numDays).round();
+    // return avgCarbs;
+
     return 72;
   }
 
