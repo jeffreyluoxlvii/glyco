4042466e4d3a38c15e1b6eb49e931f5fb71fc6ad
jeffrey.luo <jeffrey.luo@duke.edu>
2020-10-13 19:31:47 -0700

    Made changes such that forms only take in carbs

--- glyco/lib/widgets/nutrition_form.dart
+++ glyco/lib/widgets/nutrition_form.dart
@@ -15,17 +15,9 @@ class NutritionForm extends StatefulWidget {
 }
 
 class NutritionFormState extends State<NutritionForm> {
-  final _carbsFocusNode = FocusNode();
   final _form = GlobalKey<FormState>();
 
-  var _calories;
   var _carbs;
-
-  @override
-  void dispose() {
-    _carbsFocusNode.dispose();
-    super.dispose();
-  }
 
   void _submitForm() {
     final isValid = _form.currentState.validate();
@@ -33,8 +25,7 @@ class NutritionFormState extends State<NutritionForm> {
       return;
     }
     _form.currentState.save();
-    Provider.of<Measurement>(context, listen: false)
-        .addNutrition(_calories, _carbs);
+    Provider.of<Measurement>(context, listen: false).addNutrition(_carbs);
     Navigator.of(context).pop();
   }
 
@@ -71,45 +62,10 @@ class NutritionFormState extends State<NutritionForm> {
                   return null;
                 },
                 onSaved: (value) {
-                  _calories = int.parse(value);
-                },
-                textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.number,
-                onFieldSubmitted: (_) {
-                  FocusScope.of(context).requestFocus(_carbsFocusNode);
-                },
-                decoration: InputDecoration(
-                  labelText: 'Enter calories',
-                  suffix: Text(
-                    'kcal',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a value';
-                  }
-                  if (int.tryParse(value) == null) {
-                    return 'Please enter a valid number.';
-                  }
-                  if (int.parse(value) <= 0) {
-                    return 'Please enter a value greater than 0.';
-                  }
-                  if (int.parse(value) > 9999) {
-                    return 'Please enter a value less than 10000.';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
                   _carbs = int.parse(value);
                 },
                 textInputAction: TextInputAction.done,
                 keyboardType: TextInputType.number,
-                focusNode: _carbsFocusNode,
                 decoration: InputDecoration(
                   labelText: 'Enter carbs',
                   suffix: Text(
