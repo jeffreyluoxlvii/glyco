4503c20e9f1351943e58609dbeb3f1e39838cae8
Herleen Kaur <hk233@duke.edu>
2020-10-15 02:07:27 -0500

    added async load to challenges widget

--- glyco/lib/widgets/challenge_container.dart
+++ glyco/lib/widgets/challenge_container.dart
@@ -3,9 +3,12 @@ import 'package:provider/provider.dart';
 import '../providers/measurements.dart';
 
 class ChallengeContainer extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    Future<void> refresh() async {
+      await Provider.of<Measurements>(context, listen: false);
+    }
+
     final challengesProvider = Provider.of<Measurements>(context);
 
     return Center(
@@ -14,8 +17,7 @@ class ChallengeContainer extends StatelessWidget {
             padding: const EdgeInsets.all(16),
             child: Row(children: [
               Flexible(
-                child: Text(
-                    challengesProvider.generateChallenge(),
+                child: Text(challengesProvider.generateChallenge(),
                     style: TextStyle(
                       color: Colors.black,
                       fontSize: 14,
