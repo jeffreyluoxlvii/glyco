e9df0e88537abf7bd61fa1e20efb85c317eab1a3
Justin Wu <jaw117@duke.edu>
2020-10-30 21:35:12 -0600

    Finished changes to the settings screen

--- glyco/lib/widgets/shortcuts/shortcuts_summary.dart
+++ glyco/lib/widgets/shortcuts/shortcuts_summary.dart
@@ -1,139 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:glyco/providers/settings.dart';
 import 'package:provider/provider.dart';
+import '../../providers/options.dart';
+import '../../widgets/shortcuts/nutrition_shortcut_form.dart';
+import '../../widgets/shortcuts/exercise_shortcut_form.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ShortcutsSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final options = Provider.of<Options>(context);
+    InkWell editShortcut(icon, label, measurement) {
+      return InkWell(
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: Theme.of(context).primaryColor,
+              size: 45,
+            ),
+            Column(
+              children: [
+                Text(
+                  label,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(11, 0, 11, 0),
+                  child: measurement,
+                ),
+              ],
+            ),
+          ],
+        ),
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                content: SingleChildScrollView(
+                  child: ChangeNotifierProvider.value(
+                    child: label == "ACTIVITY" ? ExerciseShortcutForm() : ShortcutForm(icon, label),
+                    value: options,
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      );
+    }
+
     final settings = Provider.of<Settings>(context);
     return Column(
       children: [
         Row(
           children: [
-            InkWell(
-              child: Row(
-                children: [
-                  Icon(
-                    FontAwesomeIcons.hamburger,
-                    color: Theme.of(context).primaryColor,
-                    size: 45,
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "MEAL",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(11, 0, 11, 0),
-                        child: Text(" ${settings.mealCarbs}g carbs"),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              onTap: () {
-                print("Burger");
-              },
-            ),
+            editShortcut(FontAwesomeIcons.hamburger, "MEAL",
+                Text(" ${settings.mealCarbs}g carbs")),
             Spacer(),
-            InkWell(
-              child: Row(
-                children: [
-                  Icon(
-                    FontAwesomeIcons.cookie,
-                    color: Theme.of(context).primaryColor,
-                    size: 45,
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "SNACK",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(11, 0, 11, 0),
-                        child: Text(" ${settings.snackCarbs}g carbs"),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              onTap: () {
-                print("Cookie");
-              },
-            ),
+            editShortcut(FontAwesomeIcons.cookie, "SNACK",
+                Text(" ${settings.snackCarbs}g carbs")),
           ],
         ),
         SizedBox(height: 15),
         Row(
           children: [
-            InkWell(
-              child: Row(
-                children: [
-                  Icon(
-                    FontAwesomeIcons.mugHot,
-                    color: Theme.of(context).primaryColor,
-                    size: 45,
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "DRINK",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(11, 0, 11, 0),
-                        child: Text(" ${settings.drinkCarbs}g carbs"),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              onTap: () {
-                print("Drink");
-              },
-            ),
+            editShortcut(FontAwesomeIcons.mugHot, "DRINK",
+                Text(" ${settings.drinkCarbs}g carbs")),
             Spacer(),
-            InkWell(
-              child: Row(
-                children: [
-                  Icon(
-                    FontAwesomeIcons.running,
-                    color: Theme.of(context).primaryColor,
-                    size: 45,
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "ACTIVITY",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(11, 0, 11, 0),
-                        child: Text("${settings.exerciseTime} minutes"),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              onTap: () {
-                print("Run");
-              },
-            ),
+            editShortcut(FontAwesomeIcons.running, "ACTIVITY",
+                Text("${settings.exerciseTime} minutes")),
           ],
         ),
         SizedBox(height: 15),
