ebb27edc9319908524e971bf57bf06a5a7e9f841
Justin Wu <jaw117@duke.edu>
2020-11-13 20:48:02 -0600

    Made some progress with tests

--- glyco/test/providers/widget_tests/users_tests/settings_tests/change_shortcut_forms_test.dart
+++ glyco/test/providers/widget_tests/users_tests/settings_tests/change_shortcut_forms_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import 'package:glyco/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 import '../../../../../lib/providers/options.dart';
 import '../../../../../lib/providers/settings.dart';
@@ -12,51 +13,70 @@ class MockOptions extends Mock implements Options {}
 
 class MockSettings extends Mock implements Settings {}
 
-void main() async{
-
+void main() async {
   Options mockOptions;
   Settings mockSettings;
-  setUp((){
+  setUp(() {
     mockOptions = MockOptions();
     mockSettings = MockSettings();
-    mockSettings.drinkCarbs = 20;
-    mockSettings.exerciseTime = 20;
-    mockSettings.mealCarbs = 20;
-    mockSettings.snackCarbs = 20;
-
+    when(mockSettings.drinkCarbs).thenReturn(15);
+    when(mockSettings.exerciseTime).thenReturn(45);
+    when(mockSettings.mealCarbs).thenReturn(50);
+    when(mockSettings.snackCarbs).thenReturn(25);
+    when(mockOptions.settings).thenReturn(Settings(
+        drinkCarbs: 15,
+        exerciseTime: 45,
+        mealCarbs: 50,
+        snackCarbs: 25,
+        userId: "123456"));
   });
 
+  Widget wrapWithMaterial() => MaterialApp(
+        home: Provider<Options>(
+          create: (_) => mockOptions,
+          child: ExerciseShortcutForm(),
+        ),
+      );
+
   Future<void> _buildExerciseForm(WidgetTester tester) async {
-    // await tester.pumpWidget(
-    //   ChangeNotifierProvider<Options>.value(
-    //     value: MockOptions(),
-    //     child: ShortcutsSummary(),
-    //   ),
-
-    // );
-    // await tester.pumpWidget(
-    //   ChangeNotifierProvider<Settings>.value(
-    //     value: MockSettings(),
-    //     child: ShortcutsSummary(),
-    //   ),
-
-    // );
     await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider<Options>(
-            create: (_) => mockOptions,
+      MaterialApp(
+        home: ChangeNotifierProvider<Options>.value(
+          value: mockOptions,
+          child: Column(
+            children: [
+              ExerciseShortcutForm(),
+            ],
           ),
-          ChangeNotifierProvider<Settings>(
-            create: (_) => mockSettings,
-          ),
-        ],
-        child: Builder(
-          builder: (_) => ShortcutsSummary(),
         ),
       ),
     );
   }
+
+  // );
+  // await tester.pumpWidget(
+  //   ChangeNotifierProvider<Settings>.value(
+  //     value: MockSettings(),
+  //     child: ShortcutsSummary(),
+  //   ),
+
+  // );
+  // await tester.pumpWidget(
+  //   MultiProvider(
+  //     providers: [
+  //       ChangeNotifierProvider<Options>(
+  //         create: (_) => mockOptions,
+  //       ),
+  //       ChangeNotifierProvider<Settings>(
+  //         create: (_) => mockSettings,
+  //       ),
+  //     ],
+  //     child: Builder(
+  //       builder: (_) => ShortcutsSummary(),
+  //     ),
+  //   ),
+  // );
+  // }
 
   testWidgets('Forgot password screen has a form', (WidgetTester tester) async {
     print(mockSettings.mealCarbs);
