761775e869b0015773e813bda6080cd7963ad4d7
jeffrey.luo <jeffrey.luo@duke.edu>
2020-11-14 20:31:07 -0800

    Created tests for measures screen widgets

---
+++ glyco/test/widgets/measures_tests/forms_test.dart
@@ -0,0 +1,181 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:glyco/providers/measurement.dart';
+import 'package:glyco/providers/measurements.dart';
+import 'package:glyco/widgets/forms/exercise_form.dart';
+import 'package:glyco/widgets/forms/exercise_manual_form.dart';
+import 'package:glyco/widgets/forms/glucose_manual_form.dart';
+import 'package:glyco/widgets/forms/nutrition_form.dart';
+import 'package:glyco/widgets/forms/nutrition_manual_form.dart';
+import 'package:provider/provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mockito/mockito.dart';
+
+class MockMeasurements extends Mock implements Measurements {}
+
+class MockMeasurement extends Mock implements Measurement {}
+
+void main() async {
+  Measurements mockMeasurements;
+  Measurement mockMeasurement;
+  setUp(() {
+    mockMeasurements = MockMeasurements();
+    mockMeasurement = MockMeasurement();
+    when(mockMeasurement.a1cLevel).thenReturn(0);
+    when(mockMeasurement.avgGlucoseLevel).thenReturn(0);
+    when(mockMeasurement.calories).thenReturn(0);
+    when(mockMeasurement.carbs).thenReturn(0);
+    when(mockMeasurement.currGlucoseLevel).thenReturn(0);
+    when(mockMeasurement.date).thenReturn(DateTime.now());
+    when(mockMeasurement.exerciseTime).thenReturn(0);
+    when(mockMeasurement.id).thenReturn('0');
+    when(mockMeasurement.lastUpdate).thenReturn(DateTime.now());
+    when(mockMeasurement.steps).thenReturn(0);
+    when(mockMeasurement.userId).thenReturn('0');
+    when(mockMeasurements.measurements).thenReturn([
+      Measurement(
+        id: '0',
+        date: DateTime.now(),
+        lastUpdate: DateTime.now(),
+        avgGlucoseLevel: 0,
+        currGlucoseLevel: 0,
+        steps: 0,
+        exerciseTime: 0,
+        calories: 0,
+        carbs: 0,
+        a1cLevel: 0,
+        userId: '0',
+      )
+    ]);
+  });
+
+  Future<void> _buildNutritionForm(WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Measurement>.value(
+            value: mockMeasurement,
+          ),
+          ChangeNotifierProvider<Measurements>.value(
+            value: mockMeasurements,
+          ),
+        ],
+        child: MaterialApp(
+          home: Scaffold(body: NutritionForm(FontAwesomeIcons.hamburger)),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _buildExerciseForm(WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Measurement>.value(
+            value: mockMeasurement,
+          ),
+          ChangeNotifierProvider<Measurements>.value(
+            value: mockMeasurements,
+          ),
+        ],
+        child: MaterialApp(
+          home: Scaffold(body: ExerciseForm()),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _buildGlucoseManualForm(WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Measurement>.value(
+            value: mockMeasurement,
+          ),
+          ChangeNotifierProvider<Measurements>.value(
+            value: mockMeasurements,
+          ),
+        ],
+        child: MaterialApp(
+          home: Scaffold(body: GlucoseManualForm()),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _buildNutritionManualForm(WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Measurement>.value(
+            value: mockMeasurement,
+          ),
+          ChangeNotifierProvider<Measurements>.value(
+            value: mockMeasurements,
+          ),
+        ],
+        child: MaterialApp(
+          home: Scaffold(body: NutritionManualForm()),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _buildExerciseManualForm(WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Measurement>.value(
+            value: mockMeasurement,
+          ),
+          ChangeNotifierProvider<Measurements>.value(
+            value: mockMeasurements,
+          ),
+        ],
+        child: MaterialApp(
+          home: Scaffold(body: ExerciseManualForm()),
+        ),
+      ),
+    );
+  }
+
+  testWidgets('Nutrition Form', (WidgetTester tester) async {
+    await _buildNutritionForm(tester);
+    expect(find.byType(Form), findsOneWidget);
+    expect(find.byType(Icon), findsOneWidget);
+    expect(find.byType(Text), findsNWidgets(4));
+    expect(find.byType(TextFormField), findsNWidgets(1));
+  });
+
+  testWidgets('Exercise Form', (WidgetTester tester) async {
+    await _buildExerciseForm(tester);
+    expect(find.byType(Form), findsOneWidget);
+    expect(find.byType(Icon), findsOneWidget);
+    expect(find.byType(Text), findsNWidgets(4));
+    expect(find.byType(TextFormField), findsNWidgets(1));
+  });
+
+  testWidgets('Glucose Manual Form', (WidgetTester tester) async {
+    await _buildGlucoseManualForm(tester);
+    expect(find.byType(Form), findsOneWidget);
+    expect(find.byType(Icon), findsOneWidget);
+    expect(find.byType(Text), findsNWidgets(4));
+    expect(find.byType(TextFormField), findsNWidgets(1));
+  });
+
+  testWidgets('Nutrition Manual Form', (WidgetTester tester) async {
+    await _buildNutritionManualForm(tester);
+    expect(find.byType(Form), findsOneWidget);
+    expect(find.byType(Icon), findsOneWidget);
+    expect(find.byType(Text), findsNWidgets(4));
+    expect(find.byType(TextFormField), findsNWidgets(1));
+  });
+
+  testWidgets('Exercise Manual Form', (WidgetTester tester) async {
+    await _buildExerciseManualForm(tester);
+    expect(find.byType(Form), findsOneWidget);
+    expect(find.byType(Icon), findsOneWidget);
+    expect(find.byType(Text), findsNWidgets(4));
+    expect(find.byType(TextFormField), findsNWidgets(1));
+  });
+}
