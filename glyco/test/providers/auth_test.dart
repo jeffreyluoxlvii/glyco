b66ee995e582e1bc86efffc269d3c7d4cb4fa2cd
Justin Wu <jaw117@duke.edu>
2020-11-15 00:34:14 -0600

    Added measurements test

--- glyco/test/providers/auth_test.dart
+++ glyco/test/providers/auth_test.dart
@@ -9,26 +9,16 @@ import 'dart:async';
 // class MockAuth extends Mock implements Auth {}
 
 void main() {
-  const email = "test@gmail.com";
-  const password = "123456";
-  const firstName = "First";
-  const lastName = "Test";
-  const token = "123123qwerty";
-  const userID = "FirstTest123";
-  const secondEmail = "test@gmail.com";
-  const secondName = "Second";
-  
+  Auth auth;
 
-  Auth mockAuth;
-
-  // setUp(() {
-  //   mockAuth = MockAuth();
-  // });
+  setUp(() {
+    auth = Auth();
+  });
 
   Future<void> _buildAuth(WidgetTester tester) async {
     await tester.pumpWidget(
       ChangeNotifierProvider<Auth>.value(
-        value: Auth(),
+        value: auth,
         child: Container(),
       ),
     );
@@ -37,49 +27,7 @@ void main() {
   testWidgets(
     'Created an account successfully',
     (WidgetTester tester) async {
-      // await _buildAuth(tester);
-      // // when(Auth().signUp(email, password, firstName, lastName)).thenAnswer();
-      // when(mockAuth.userName).thenReturn(firstName + " " + lastName);
-      // await Auth().signUp(email, password, firstName, lastName);
-      // await Auth().signIn(email, password);
-      // when(mockAuth.userEmail).thenReturn(email);
-      // when(mockAuth.userName).thenReturn(firstName + " " + lastName);
-      // when(mockAuth.token).thenReturn(token);
-      // when(mockAuth.userId).thenReturn(userID);
-      // expect(mockAuth.userEmail, email);
-      // expect(mockAuth.userName, firstName + " " + lastName);
-      // expect(mockAuth.token, token);
-      // expect(mockAuth.userId, userID);
+      await _buildAuth(tester);
     },
   );
-
-  // testWidgets(
-  //   'Changed profile settings successfully',
-  //   (WidgetTester tester) async {
-  //     await _buildAuth(tester);
-  //     await mockAuth.changeName(secondName + " " +lastName);
-  //     when(mockAuth.userName).thenReturn(secondName + " " + lastName);
-  //     expect(mockAuth.userName, secondName + " " + lastName);
-
-  //     await mockAuth.changeProfile("email", secondEmail);
-  //     when(mockAuth.userEmail).thenReturn(secondEmail);
-  //     expect(mockAuth.userEmail, secondEmail);
-  //   },
-  // );
-
-  // testWidgets(
-  //   'Logged out successfully',
-  //   (WidgetTester tester) async {
-  //     await _buildAuth(tester);
-  //     await mockAuth.logout();
-  //     when(mockAuth.userEmail).thenReturn(null);
-  //     when(mockAuth.userName).thenReturn(null);
-  //     when(mockAuth.token).thenReturn(null);
-  //     when(mockAuth.userId).thenReturn(null);
-  //     expect(mockAuth.userEmail, null);
-  //     expect(mockAuth.userName, null);
-  //     expect(mockAuth.token, null);
-  //     expect(mockAuth.userId, null);
-  //   },
-  // );
 }
